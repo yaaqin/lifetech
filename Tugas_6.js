729b7a2bcf1f4847a59ae3d74bcbb848ae294cb5
yaaqin <yaqin.me@gmail.com>
2024-02-16 17:00:14 +0700

    finish

--- Tugas_6.js
+++ Tugas_6.js
@@ -1,17 +1,16 @@
-const Outlets = [
+let Outlets = [
   { id: 11, name: "Hik Jo" },
-  { id: 12, name: "Outlet 1" },
+  { id: 12, name: "Outlet 1" }
 ];
 
-const product = [
+let product = [
   { id: 1, outlet_id: 11, name: 'Bakso Bakar' },
   { id: 2, outlet_id: 11, name: 'Bakso Goreng' },
-  { id: 3, outlet_id: 12, name: 'Es Teh' },
+  { id: 3, outlet_id: 12, name: 'Es Teh' }
 ];
 
 Outlets[0].product = [product[0],product[1]]
-Outlets[1].product = product[2]
-
+Outlets[1].product = [product[2]]
 
 
 
